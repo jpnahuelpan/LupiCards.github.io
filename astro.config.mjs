cc69b1252d7b4fb7ffd2727fe287c585caa1aff9
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-11-26 15:27:36 -0300

    chore: add tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lupicards.github.io',
-  base: 'LupiCards.github.io'
+  base: 'LupiCards.github.io',
+  integrations: [tailwind()]
 });
