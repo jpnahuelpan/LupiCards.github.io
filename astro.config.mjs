bf3143601d153463137e1b808d9059e62dee47ff
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-11-26 12:38:10 -0300

    feat: add deploy config for ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lupicards.github.io',
+  base: 'LupiCards.github.io'
+});
