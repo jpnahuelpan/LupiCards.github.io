4c60fc283425d4d18b531275617bd5bf48b194c5
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-11-26 16:28:48 -0300

    fix: base route for index

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lupicards.github.io',
-  base: 'LupiCards.github.io',
+  base: '/',
   integrations: [tailwind()]
 });
